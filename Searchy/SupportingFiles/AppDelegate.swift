c5d8c9d43d02b43422f880a2b924b74327b60078
Yiğit Erdinç <yigiterdinc@gmail.com>
2023-07-23 18:23:02 +0300

    Added firebase messaging to project

--- Searchy/SupportingFiles/AppDelegate.swift
+++ Searchy/SupportingFiles/AppDelegate.swift
@@ -5,17 +5,32 @@
 //  Created by Yiğit Erdinç on 20.07.2023.
 //
 
-import FirebaseCore
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
+
     NetworkReachability.shared.startListening()
+
+    Messaging.messaging().delegate = self
+    UNUserNotificationCenter.current().delegate = self
+
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { authorizationStatus, error in
+      if let error = error {
+        print("Error registering APNs Token: \(error)")
+      } else {
+        print("APNs Token successfully registered, status: \(authorizationStatus)")
+      }
+    }
+
+    application.registerForRemoteNotifications()
     return true
   }
 
@@ -29,5 +44,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
   func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+  }
+}
+
+extension AppDelegate: MessagingDelegate {
+  func messaging(
+    _ messaging: Messaging,
+    didReceiveRegistrationToken fcmToken: String?
+  ) {
+    messaging.token { registeredToken, error in
+      if let error = error {
+        print("Error registering FCM Token: \(error)")
+      } else if let registeredToken = registeredToken {
+        print("FCM Token successfully registered: \(registeredToken)")
+      }
+    }
   }
 }
